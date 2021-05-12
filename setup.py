b7370dafb34294f714d4cc7f83d3fe69b74fd00c
Carson <wilde.utah@gmail.com>
2021-05-11 23:08:35 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
-from setuptools import setup
+# from setuptools import setup
 
-def readme():
-    with open('README.rst') as f:
-        return f.read()
+# def readme():
+#     with open('README.rst') as f:
+#         return f.read()
 
-setup(name='rpi_utils',
-      version='1.0',
-      description='A Python module for various raspberry pi project utilities.',
-      classifiers=[
-          'Topic :: Software Development :: Libraries',
-          'Programming Language :: Python :: 3',
-          'Operating System :: POSIX :: Linux',
-      ],
-      keywords='stepper motor mpu6050 raspberry',
-      url='https://github.com/Cwilde921/rpi_utils',
-      author='Cwilde921',
-      author_email='wilde.utah@gmail.com',
-      license='MIT',
-      packages=['rpi_utils'],
-      zip_safe=False,
-    )
+# setup(name='rpi_utils',
+#       version='1.0',
+#       description='A Python module for various raspberry pi project utilities.',
+#       classifiers=[
+#           'Topic :: Software Development :: Libraries',
+#           'Programming Language :: Python :: 3',
+#           'Operating System :: POSIX :: Linux',
+#       ],
+#       keywords='stepper motor mpu6050 raspberry',
+#       url='https://github.com/Cwilde921/rpi_utils',
+#       author='Cwilde921',
+#       author_email='wilde.utah@gmail.com',
+#       license='MIT',
+#       packages=['rpi_utils'],
+#       zip_safe=False,
+#     )
