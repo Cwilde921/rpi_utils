4256d97bcacb990d6e50adab87e43fc8976f1cae
= <=>
2021-05-11 23:02:09 -0600

    bug fixes in byj48.py

--- rpi_utils/byj48.py
+++ rpi_utils/byj48.py
@@ -23,19 +23,15 @@ class Motor:
         self._write_pins(self.__step_seq.current())
 
     def step(self, stp:int):
-        if self.flip_dir:
-            stp = -stp
-        if   stp ==  0: return self.release_break()
-        if   stp ==  1: return self._write_pins(self.__step_seq.next(Dir.CW))
-        elif stp == -1: return self._write_pins(self.__step_seq.next(Dir.CCW))
+        if stp ==  0: 
+            return
+        self._write_pins(self.__step_seq.next(stp))
 
     def _write_pins(self, outpt:List[bool]):
         for i in range(len(self.__pins)):
             GPIO.output(self.__pins[i], GPIO.HIGH if outpt[i] else GPIO.LOW)
 
-    def walk(self, steps:int, delay):
-        if self.flip_dir:
-            steps = -steps
+    def walk(self, steps:int, delay=0.003):
         if steps > 0:
             dir = 1
         else:
@@ -46,7 +42,7 @@ class Motor:
             sleep(delay)
         self.release_break()
 
-    def seps_in_rot(self):
+    def get_seps_in_rotation(self):
         return self.__step_seq.steps_in_rot()
 
 class StepSequence:
@@ -54,13 +50,7 @@ class StepSequence:
     FOUR = 4
     EIGHT = 8
 
-    def __init__(self, step_seq:int, reverse_dir:bool=False):
-        if reverse_dir:
-            self.CW = -1
-            self.CCW = 1
-        else:
-            self.CW = 1
-            self.CCW = -1
+    def __init__(self, step_seq:int):
         if step_seq == self.FOUR:
             self.step_seq = self.FOUR
         elif step_seq == self.EIGHT:
@@ -79,35 +69,37 @@ class StepSequence:
             ]
         self._step_ctr = 0
 
-    def next(self, dir:Dir):
+    def next(self, dir:int):
         self._update_ctr(dir)
         return self.steps[self._step_ctr]
 
     def current(self):
         return self.steps[self._step_ctr]
 
-    def _update_ctr(self, dir:Dir):
+    def _update_ctr(self, dir:int):
         stp = 1 if self.step_seq == self.EIGHT else 2
-        if(dir == Dir.CW):
+        if(dir == 1):
             self._step_ctr += stp
             if(self._step_ctr >= len(self.steps)):
                 self._step_ctr = 0
-        else:
+        elif(dir == -1):
             self._step_ctr -= stp
             if(self._step_ctr < 0):
                 self._step_ctr = len(self.steps) -stp
+        else:
+            raise ValueError("dir must be 1 or -1, got {}".format(dir))
 
-    def steps_in_rot(self):
+    def get_steps_in_rotation(self):
         return 2048 if self.steps == self.FOUR else 4096
 
 
 if __name__ == "__main__":
     ss = StepSequence(StepSequence.FOUR)
     for i in range(20):
-        print(ss.next(Dir.CW))
+        print(ss.next(1))
     print("changing direction")
     for i in range(15):
-        print(ss.next(Dir.CCW))
+        print(ss.next(-1))
 
     m1 = Motor(config["r_motor_pins_bcm"], True)
     m1.walk(12048, 0.003)
